39efce2432fd90de07719c2380d604085f0ca887
Etienne Tremel <etienne.tremel@orange.fr>
2022-04-12 15:10:02 +0200

    fix: revert GitHub token authentication

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -113,7 +113,6 @@ type Github struct {
 	AppID          int64  `mapstructure:"appID"`
 	InstallationID int64  `mapstructure:"installationID"`
 	PrivateKeyPath string `mapstructure:"privateKeyPath"`
-	Token          string `mapstructure:"token"`
 }
 
 // Gitlab define Gitlab configuration
